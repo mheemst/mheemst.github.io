2b6eeaae702ddcf1e26db0c33e739cdfc9efa936
mheemst <45948037+mheemst@users.noreply.github.com>
2019-01-28 21:12:12 -0500

    Changed php back to html

--- includes/navigation-mobile.php
+++
@@ -1,4 +0,0 @@
-<h2>Navigation</h2>
-				<a href="index.php" class="mobile-nav-link">Portfolio</a> <br/>
-				<a href="about.php" class="mobile-nav-link">About Me</a> <br/>
-				<a href="resume.php" class="mobile-nav-link">Resume</a> <br/>
--- includes/navigation.php
+++
@@ -1,5 +0,0 @@
-<h1>Makayla Heemstra</h1>
-	<h2>Navigation</h2>
-		<a href="index.php">Portfolio</a> <br/>
-		<a href="about.php">About Me</a> <br/>
-		<a href="resume.php">Resume</a> <br/>
